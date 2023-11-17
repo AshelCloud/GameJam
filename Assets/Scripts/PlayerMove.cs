9028350b23a8be0890dfb7726689c06863d75ae3
YoungJunGit <eimuc00@gmail.com>
2023-11-18 00:32:27 +0900

    플레이어 이동 벽 감지(미구현)
    .

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,14 +6,20 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Transform tr;
 
     [SerializeField]
     float playerSpeed = 1;
+
+    private bool isJumping = false;
+    private bool isClimbing = false;
+    public bool canClimb = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -37,7 +43,47 @@ public class PlayerMove : MonoBehaviour
         }
 
         moveInput.x = x;
+
+        Vector3 tempTr = tr.position + moveInput;
+
+        if (DetectWall(tempTr))
+        {
+            tr.Translate(moveInput * Time.deltaTime * playerSpeed);
+        }
+    }
+
+    void Jump()
+    {
+        if (isJumping)
+            return;
+
+        if(isClimbing) 
+        { 
+            
+        }
+        else
+        {
+
+        }
+    }
+
+    void Climb()
+    {
+
+    }
+
+    bool DetectWall(Vector3 tr)
+    {
         
-        transform.position += moveInput * Time.deltaTime * playerSpeed;
+
+        return false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "CLIMBWALL")
+        {
+            canClimb = true;
+        }
     }
 }
