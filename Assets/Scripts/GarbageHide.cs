00d10eae047318c268c2a165613d185e157cf4fe
Danny <83389499+danielhjpark@users.noreply.github.com>
2023-11-18 07:35:45 +0900

    NEW:쓰래기숨기 구현

---
+++ Assets/Scripts/GarbageHide.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageHide : MonoBehaviour
+{
+    public Player player;
+    private bool isPlayerActive = true;
+    [SerializeField]
+    private bool hasCollided = false;
+
+    void Start()
+    {
+
+        if (player == null)
+        {
+            Debug.LogError("Player reference not assigned!");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D Collider)
+    {
+        if (Collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("충돌함");
+            hasCollided = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D Collider)
+    {
+        if(player.gameObject.activeSelf == true)
+        {
+            if (Collider.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("충돌종료");
+                hasCollided = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (hasCollided && Input.GetKeyDown(KeyCode.E))
+        {
+            if (isPlayerActive)
+            {
+                MakePlayerDisappear();
+            }
+            else
+            {
+                MakePlayerAppear();
+            }
+            Vector3 currentScale = player.transform.localScale;
+            currentScale.x = 1f;
+            player.transform.localScale = currentScale;
+        }
+    }
+
+    void MakePlayerDisappear()
+    {
+        // 플레이어를 비활성화하거나 숨긴다
+        player.gameObject.SetActive(false);
+        isPlayerActive = false;
+    }
+
+    void MakePlayerAppear()
+    {
+        // 플레이어를 활성화하거나 다시 보이게 한다
+        player.gameObject.SetActive(true);
+        isPlayerActive = true;
+    }
+}
