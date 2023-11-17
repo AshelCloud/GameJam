ccdf22a318aa0c943a4a6acdd64d30716f4c1e68
YoungJunGit <eimuc00@gmail.com>
2023-11-18 01:20:55 +0900

    플레이어 이동(벽뚫문제)
    .

--- Assets/Scenes/PlayerMove.cs
+++ Assets/Scenes/PlayerMove.cs
@@ -7,19 +7,25 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rigid;
     Transform tr;
+    BoxCollider2D boxCollider;
 
     [SerializeField]
     float playerSpeed = 1;
 
+    [SerializeField]
+    float perceptionRange = 1f;
+
     private bool isJumping = false;
     private bool isClimbing = false;
     public bool canClimb = false;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         tr = GetComponent<Transform>();
+        boxCollider= GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -44,12 +50,13 @@ public class PlayerMove : MonoBehaviour
 
         moveInput.x = x;
 
-        Vector3 tempTr = tr.position + moveInput;
-
-        if (DetectWall(tempTr))
+        if (CheckHitWall(moveInput))
         {
-            tr.Translate(moveInput * Time.deltaTime * playerSpeed);
+            Debug.Log("벽 감지");
+            moveInput = Vector3.zero;
         }
+
+        tr.Translate(moveInput * Time.deltaTime * playerSpeed);
     }
 
     void Jump()
@@ -72,10 +79,34 @@ public class PlayerMove : MonoBehaviour
 
     }
 
-    bool DetectWall(Vector3 tr)
+    bool CheckHitWall(Vector3 movement)
     {
-        
+        // 움직임에 대한 로컬 벡터를 월드 벡터로 변환해준다.
+        movement = transform.TransformDirection(movement);
+        // scope로 ray 충돌을 확인할 범위를 지정할 수 있다.
+        float scope = 1f;
 
+        // 플레이어의 머리, 가슴, 발 총 3군데에서 ray를 쏜다.
+        List<Vector3> rayPositions = new List<Vector3>();
+        rayPositions.Add(transform.position + Vector3.up * 0.1f);
+        rayPositions.Add(transform.position + Vector3.up * boxCollider.size.y * 0.5f);
+        rayPositions.Add(transform.position + Vector3.up * boxCollider.size.y);
+
+        // 디버깅을 위해 ray를 화면에 그린다.
+        foreach (Vector3 pos in rayPositions)
+        {
+            Debug.DrawRay(pos, movement * scope, Color.red);
+        }
+
+        // ray와 벽의 충돌을 확인한다.
+        foreach (Vector3 pos in rayPositions)
+        {
+            if (Physics.Raycast(pos, movement, out RaycastHit hit, scope))
+            {
+                if (hit.collider.CompareTag("WALL"))
+                    return true;
+            }
+        }
         return false;
     }
 
