180bab7f4be9dfb866a3d6e7d02bc3d3bb2e005b
YoungJunGit <eimuc00@gmail.com>
2023-11-18 20:52:38 +0900

    CCTV 구현 완
    .

--- Assets/Scripts/HackObject/DroneController.cs
+++ Assets/Scripts/HackObject/DroneController.cs
@@ -10,16 +10,21 @@ public class DroneController : HackableObject
     [SerializeField]
     List<GameObject> m_ControlledDrones;
 
+    [SerializeField] Color hackedColor;
+
     protected override void Hacking()
     {
         base.Hacking();
 
         transform.GetComponent<SpriteRenderer>().sprite = sprites[1];
+        transform.GetComponent<SpriteRenderer>().color = hackedColor;
 
         foreach(var controlledDrone in m_ControlledDrones)
         {
             controlledDrone.GetComponent<Drone>().enabled = false;
             controlledDrone.transform.Find("PerceptionRange").gameObject.SetActive(false);
         }
+
+        this.enabled = false;
     }
 }
